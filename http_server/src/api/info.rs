86ac5770a369fd086d0ce0706a998cb5d940ce98
Max Vissing <max_vissing@yahoo.de>
2024-05-03 16:33:02 +0200

    Receive openid

--- http_server/src/api/info.rs
+++ http_server/src/api/info.rs
@@ -11,6 +11,12 @@ pub struct Info {
     pub version: String,
     pub openid_configuration_url: String,
     pub client_id: String,
+    pub authorize_url: String,
+    pub token_url: String,
+    pub response_types_supported: Vec<String>,
+    pub scopes_supported: Vec<String>,
+    pub claims_supported: Vec<String>,
+    pub code_challenge_methods_supported: Vec<String>,
 }
 
 pub async fn info(
@@ -20,8 +26,23 @@ pub async fn info(
         StatusCode::OK,
         Json::from(Info {
             version: String::from(VERSION),
-            openid_configuration_url: server_config.openid_configuration_url.clone(),
+            openid_configuration_url: server_config.openid_url.clone(),
             client_id: server_config.client_id.clone(),
+            authorize_url: server_config
+                .openid_configuration
+                .authorization_endpoint
+                .clone(),
+            token_url: server_config.openid_configuration.token_endpoint.clone(),
+            response_types_supported: server_config
+                .openid_configuration
+                .response_types_supported
+                .clone(),
+            scopes_supported: server_config.openid_configuration.scopes_supported.clone(),
+            claims_supported: server_config.openid_configuration.claims_supported.clone(),
+            code_challenge_methods_supported: server_config
+                .openid_configuration
+                .code_challenge_methods_supported
+                .clone(),
         }),
     ))
 }
--- http_server/src/config.rs
+++ http_server/src/config.rs
@@ -2,28 +2,45 @@ use serde::{Deserialize, Serialize};
 use snafu::{ResultExt, Whatever};
 
 const ENV_OAUTH_CLIENT_ID: &str = "OAUTH_CLIENT_ID";
-const ENV_OAUTH_JWKS_URL: &str = "OAUTH_JWKS_URL";
 const ENV_OPENID_CONFIGURATION_URL: &str = "OPENID_CONFIGURATION_URL";
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 pub struct ServerConfig {
     pub client_id: String,
-    pub jwks_url: String,
-    pub openid_configuration_url: String,
+    pub openid_url: String,
+    pub openid_configuration: OpenIdDiscovery,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct OpenIdDiscovery {
+    pub issuer: String,
+    pub authorization_endpoint: String,
+    pub token_endpoint: String,
+    pub response_types_supported: Vec<String>,
+    pub jwks_uri: String,
+    pub scopes_supported: Vec<String>,
+    pub claims_supported: Vec<String>,
+    pub code_challenge_methods_supported: Vec<String>,
 }
 
 impl ServerConfig {
-    pub fn load() -> Result<Self, Whatever> {
+    pub async fn load() -> Result<Self, Whatever> {
         let client_id = std::env::var(ENV_OAUTH_CLIENT_ID)
             .whatever_context("Could not find oauth client id")?;
-        let jwks_url =
-            std::env::var(ENV_OAUTH_JWKS_URL).whatever_context("Could not find jwks url")?;
         let openid_configuration_url = std::env::var(ENV_OPENID_CONFIGURATION_URL)
             .whatever_context("Could not find openid configuration url")?;
+
+        let discovery = reqwest::get(openid_configuration_url.clone())
+            .await
+            .whatever_context("Could not send request to openid")?
+            .json::<OpenIdDiscovery>()
+            .await
+            .whatever_context("Could not send request to openid")?;
+
         let config = Self {
             client_id,
-            jwks_url,
-            openid_configuration_url,
+            openid_url: openid_configuration_url,
+            openid_configuration: discovery,
         };
         Ok(config)
     }
